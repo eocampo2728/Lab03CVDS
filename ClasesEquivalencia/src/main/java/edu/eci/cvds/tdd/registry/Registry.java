5bce9f617112d0937a6b07ef27223176adac04ee
Juan David Navarro Jimenez <juan.navarro@mail.escuelaing.edu.co>
2019-08-30 12:19:05 -0500

    Add documentation

--- ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
+++ ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -2,10 +2,20 @@ package edu.eci.cvds.tdd.registry;
 
 import java.util.ArrayList;
 
+/**
+ * Clase registro
+ * @author Eduardo Ocampo & Juan Navarro
+ */
 public class Registry {
     
+    //Atributos
     private static ArrayList<Person> persons = new ArrayList<>();
     
+    /**
+     * Metodo registerVoter es un metodo para registrar un voto.
+     * @param p Es la persona la cual quire registrar su voto.
+     * @return  Retorna que tipo de voto fue ("Valid","Dead","Underage","Invalid_Age","Invalid_Name","Duplicated").
+     */
     public RegisterResult registerVoter(Person p) {
         RegisterResult isValid = RegisterResult.VALID;
         if(!p.isAlive()){
